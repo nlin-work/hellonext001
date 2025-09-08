3744621ade863167aa8a87599f49062d01f784aa
nlin <norman.lin@spiralmind.jp>
2025-09-08 23:09:51 +0900

    Updates

--- _next/static/ZCyoLN0hj3LE6UWsgUPU8/_buildManifest.js
+++ _next/static/ZCyoLN0hj3LE6UWsgUPU8/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/22f805491aa2a4cf.js"
+    "./static/chunks/acd75490373cfe24.js"
   ],
   "__rewrites": {
     "afterFiles": [],
---
+++ _next/static/chunks/acd75490373cfe24.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/281bc9f30b9e2548.js",
+  "static/chunks/turbopack-69c9eae8a18a49f2.js"
+])
